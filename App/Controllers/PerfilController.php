d6fa589b8e8c8bca71a9f0ff0aff05471a2d6a6f
Albert Rocas <albertrocas02@gmail.com>
2023-11-25 13:17:13 +0100

    format perfilController.php

--- App/Controllers/PerfilController.php
+++ App/Controllers/PerfilController.php
@@ -5,33 +5,26 @@ namespace App\Controllers;
 class PerfilController
 {
     public function perfil($request, $response, $container)
-        {
-            $response->SetTemplate("PerfilView.php");
+    {
+        $response->SetTemplate("PerfilView.php");
 
-            $userId = $_SESSION["user"]["Id"];
-            $userModel = $container->get("users");
+        $userId = $_SESSION["user"]["Id"];
+        $userModel = $container->get("users");
 
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_REQUEST["action"])) {
-                $action = $_REQUEST["action"];
-                
-                if ($action === "setDefaultPhoto") {
-                    $userModel->setDefaultPhoto($userId, $_POST['idPhoto']);
-                }
-                }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_REQUEST["action"])) {
+            $action = $_REQUEST["action"];
 
-            $user = $userModel->getUserById($userId);
-            $userPhotos = $userModel->getPhotos($userId);
-            
-            $response->set("user", $user);
-            $response->set("userPhotos", $userPhotos);
-
-            
-            
-            
-            
-            // error_log(print_r($userPhotos, true));
-            // var_dump($_SESSION);
-
-            return $response;
+            if ($action === "setDefaultPhoto") {
+                $userModel->setDefaultPhoto($userId, $_POST['idPhoto']);
+            }
         }
+
+        $user = $userModel->getUserById($userId);
+        $userPhotos = $userModel->getPhotos($userId);
+
+        $response->set("user", $user);
+        $response->set("userPhotos", $userPhotos);
+
+        return $response;
+    }
 }
