e87d5f20ba28d22e8c24bf86f9724e4f011685d3
Albert Rocas <albertrocas02@gmail.com>
2023-12-12 02:09:37 +0100

    arreglo

--- App/Views/equipDirectiuView.php
+++ App/Views/equipDirectiuView.php
@@ -207,33 +207,35 @@
                     </div>
                 </div>
             <?php endforeach; ?>
-        </div>
-        <div id="pagina-reportsEd" class="paginaEd hidden">
-            <p class="text-lg font-medium text-gray-900 dark:text-white" style="margin-bottom: 15px">Imatges
-                Reportades</p>
-            <?php foreach ($reportedImages as $image): ?>
-                <div class="bg-white p-4 mb-4 border border-gray-300 rounded">
-                    <div class="flex items-center mb-2">
-                        <p class="font-bold">Nom de l'usuari:
-                            <?php echo $image['user_name']; ?>
-                        </p>
-                    </div>
-
-                    <div class="flex items-end">
-                        <div class="mr-4">
-                            <img width="100px" height="100px" src="<?php echo $image['photography_link']; ?>"
-                                alt="Reported Image" class="mt-2 rounded-lg">
+            <div id="pagina-reportsEd" class="paginaEd hidden">
+                <p class="text-lg font-medium text-gray-900 dark:text-white" style="margin-bottom: 15px">Imatges
+                    Reportades</p>
+                <?php foreach ($reportedImages as $image): ?>
+                    <div class="bg-white p-4 mb-4 border border-gray-300 rounded">
+                        <div class="flex items-center mb-2">
+                            <p class="font-bold">Nom de l'usuari:
+                                <?php echo $image['user_name']; ?>
+                            </p>
                         </div>
 
-                        <div class="ml-auto">
-                            <a href="/equipDirectiu?action=deleteReport&report_id=<?php echo $image['report_id']; ?>"
-                                class="report-button text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-end px-5 py-2 text-center">
-                                Delete Report
-                            </a>
+                        <div class="flex items-end">
+                            <div class="mr-4">
+                                <img width="100px" height="100px" src="<?php echo $image['photography_link']; ?>"
+                                    alt="Reported Image" class="mt-2 rounded-lg">
+                            </div>
+
+                            <div class="ml-auto">
+                                <a href="/equipDirectiu?action=deleteReport&report_id=<?php echo $image['report_id']; ?>"
+                                    class="report-button text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-end px-5 py-2 text-center">
+                                    Delete Report
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+
+
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
     </div>
