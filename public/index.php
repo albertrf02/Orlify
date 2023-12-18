844d4cc91a42f17e7c73d921b61b7fc7e4e31ab3
Albert Rocas <albertrocas02@gmail.com>
2023-12-18 21:51:10 +0100

    provs treure \App\ middleware

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace App;
+
 use App\Controllers\ViewsController;
 
 use App\Controllers\LoginController;
@@ -19,34 +22,34 @@ include "../vendor/autoload.php";
 header("Access-Control-Allow-Origin: *");
 
 /* Creem els diferents models */
-$contenidor = new \App\Container(__DIR__ . "/../App/config.php");
+$contenidor = new Container(__DIR__ . "/../App/config.php");
 
 $app = new \Emeset\Emeset($contenidor);
 
-$app->get("/", [ViewsController::class, "index"], [[\App\Middleware\LoadUserData::class, "loadUserData"]]);
+$app->get("/", [ViewsController::class, "index"], [[Middleware\LoadUserData::class, "loadUserData"]]);
 $app->get("/login", [ViewsController::class, "login"]);
 $app->get("/register", [ViewsController::class, "register"]);
 $app->get("/logout", [LoginController::class, "logout"]);
 $app->get("/publicOrles", [ViewsController::class, "publicOrles"]);
-$app->get("/admin", [ViewsController::class, "admin"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/equipDirectiu", [ViewsController::class, "equipDirectiu"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/perfil", [ViewsController::class, "perfil"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/canviarContrasenya", [ViewsController::class, "canviarContrasenya"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/carnet", [ViewsController::class, "carnet"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/orles", [ViewsController::class, "orles"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/veureOrla", [OrlaController::class, "getUsersFromOrla"], [[\App\Middleware\Auth::class, "auth"]]);
+$app->get("/admin", [ViewsController::class, "admin"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/equipDirectiu", [ViewsController::class, "equipDirectiu"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/perfil", [ViewsController::class, "perfil"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/canviarContrasenya", [ViewsController::class, "canviarContrasenya"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/carnet", [ViewsController::class, "carnet"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/orles", [ViewsController::class, "orles"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/veureOrla", [OrlaController::class, "getUsersFromOrla"], [[Middleware\Auth::class, "auth"]]);
 
-$app->get("/orla/edit", [OrlaController::class, "editOrla"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/orla/view", [OrlaController::class, "viewOrla"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/orla-pdf", [OrlaController::class, "orlaToPDF"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->get("/orla/iframe", [OrlaController::class, "iframeOrla"], [[\App\Middleware\Auth::class, "auth"]]);
+$app->get("/orla/edit", [OrlaController::class, "editOrla"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/orla/view", [OrlaController::class, "viewOrla"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/orla-pdf", [OrlaController::class, "orlaToPDF"], [[Middleware\Auth::class, "auth"]]);
+$app->get("/orla/iframe", [OrlaController::class, "iframeOrla"], [[Middleware\Auth::class, "auth"]]);
 
 
-$app->post("/saveOrla", [OrlaController::class, "saveOrla"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->post("/equipDirectiu", [ViewsController::class, "equipDirectiu"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->post("/report-image", [UserController::class, "reportImages"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->post("/perfil", [ViewsController::class, "perfil"], [[\App\Middleware\Auth::class, "auth"]]);
-$app->post("/updatePassword", [ViewsController::class, "canviarContrasenya"], [[\App\Middleware\Auth::class, "auth"]]);
+$app->post("/saveOrla", [OrlaController::class, "saveOrla"], [[Middleware\Auth::class, "auth"]]);
+$app->post("/equipDirectiu", [ViewsController::class, "equipDirectiu"], [[Middleware\Auth::class, "auth"]]);
+$app->post("/report-image", [UserController::class, "reportImages"], [[Middleware\Auth::class, "auth"]]);
+$app->post("/perfil", [ViewsController::class, "perfil"], [[Middleware\Auth::class, "auth"]]);
+$app->post("/updatePassword", [ViewsController::class, "canviarContrasenya"], [[Middleware\Auth::class, "auth"]]);
 $app->get("/recover", [ViewsController::class, "recover"]);
 $app->get("/invalidtoken", [ViewsController::class, "invalidtoken"]);
 $app->get("/recoverpassword/{token}", [ViewsController::class, "recoverpassword"]);
