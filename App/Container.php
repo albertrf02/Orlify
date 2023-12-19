777e560f6670a85c20509afa76cc39cf17382cc6
Albert Rocas <albertrocas02@gmail.com>
2023-12-19 15:22:53 +0100

    prova nom fitxer users.php

--- App/Container.php
+++ App/Container.php
@@ -16,17 +16,17 @@ class Container extends EmesetContainer
         if ($dbType == "PDO") {
 
             $this["users"] = function ($c) {
-                return new Models\Users($c["db"]->getConnection());
+                return new \App\Models\Users($c["db"]->getConnection());
             };
             $this["orles"] = function ($c) {
-                return new Models\Orles($c["db"]->getConnection());
+                return new \App\Models\Orles($c["db"]->getConnection());
             };
             $this["classes"] = function ($c) {
-                return new Models\Classes($c["db"]->getConnection());
+                return new \App\Models\Classes($c["db"]->getConnection());
             };
 
             $this["db"] = function ($c) {
-                return new Models\Db(
+                return new \App\Models\Db(
                     $c["config"]["db"]["user"],
                     $c["config"]["db"]["pass"],
                     $c["config"]["db"]["db"],
