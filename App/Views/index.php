a844b8171e80a1e737287286cf8c451c23a53602
Albert Rocas <albertrocas02@gmail.com>
2023-12-20 01:30:46 +0100

    css login i index

--- App/Views/index.php
+++ App/Views/index.php
@@ -10,47 +10,50 @@
   <title>Orlify</title>
 </head>
 
-<?php var_dump($_SESSION); ?>
-
-
-
-
-
-<body>
+<body class="bg-cover bg-center bg-no-repeat bg-fixed" style="background-image: url(img/classeIndex.jpg);">
   <?php require "MenuView.php" ?>
-  <?php $orlesPubliques = false; ?>
-  <?php foreach ($orles as $orla): ?>
-    <?php if ($orla['public'] == 1): ?>
-      <a href="/orla/iframe?idOrla=<?= $orla['id'] ?>"
-        class="edit-button text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-700 font-medium rounded-lg text-sm inline-flex items-end px-5 py-2 text-center">
-        Veure orla:
-        <?= $orla['name'] ?>
-        <?= $orla['className'] ?>
-      </a>
-      <?php $orlesPubliques = true; ?>
+  <div class="container mx-auto my-8">
+    <?php $orlesPubliques = false; ?>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <?php foreach ($orles as $orla): ?>
+
+        <?php if ($orla['public'] == 1): ?>
+          <div class="bg-white p-4 rounded-lg shadow-md">
+            <a href="/orla/iframe?idOrla=<?= $orla['id'] ?>" class="block text-center mb-2">
+              <img src="/img/imatge_orles.png" alt="<?= $orla['name'] ?>" class="w-full h-36 object-cover rounded-md">
+            </a>
+            <h3 class="text-lg font-medium mb-2">
+              <?= $orla['name'] ?>
+            </h3>
+            <p class="text-gray-600">
+              <?= $orla['className'] ?>
+            </p>
+          </div>
+          <?php $orlesPubliques = true; ?>
+        <?php endif ?>
+      <?php endforeach ?>
+    </div>
+    <?php if (!$orlesPubliques): ?>
+      <div class="flex flex-col items-center p-2 rounded mt-40">
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+          Actualment no hi ha cap orla pública
+        </h5>
+      </div>
     <?php endif ?>
-  <?php endforeach ?>
-  <?php if (!$orlesPubliques): ?>
-    <div class="flex flex-col items-center p-2 rounded mt-40">
-      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-        Actualment no hi ha cap orla pública
-      </h5>
-    </div>
-  <?php endif ?>
 
-
-
-
-  <div id="cookie-notification" class="fixed bottom-0 left-0 right-0 p-4 bg-gray-800 text-white flex items-center justify-center transform translate-y-full transition-transform duration-500 ease-in-out">
-    <div class="flex flex-col items-center justify-center w-full text-center rounded">
+    <div id="cookie-notification"
+      class="fixed bottom-0 left-0 right-0 p-4 bg-gray-800 text-white flex items-center justify-center transform translate-y-full transition-transform duration-500 ease-in-out">
+      <div class="flex flex-col items-center justify-center w-full text-center rounded">
         <div class="flex items-center"> <!-- Añade 'flex items-center' -->
-            <p class="text-sm my-4 mr-8">Aquest lloc web utilitza cookies per garantir que obtingueu la millor experiència en el nostre lloc web.</p> <!-- Añade 'mr-4' -->
-            <button id="accept-cookies" class="mx-2 px-4 py-2 bg-gray-700 text-white rounded shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-colors duration-200 ease-in-out">
-                Acceptar
-            </button>
+          <p class="text-sm my-4 mr-8">Aquest lloc web utilitza cookies per garantir que obtingueu la millor experiència
+            en el nostre lloc web.</p> <!-- Añade 'mr-4' -->
+          <button id="accept-cookies"
+            class="mx-2 px-4 py-2 bg-gray-700 text-white rounded shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-colors duration-200 ease-in-out">
+            Acceptar
+          </button>
         </div>
+      </div>
     </div>
-</div>
 
 
 
@@ -62,8 +65,8 @@
 
 
 
-  <script src="/js/bundle.js"></script>
-  <script src="/js/flowbite.js"></script>
+    <script src="/js/bundle.js"></script>
+    <script src="/js/flowbite.js"></script>
 </body>
 <?php require "Scripts.php" ?>
 
